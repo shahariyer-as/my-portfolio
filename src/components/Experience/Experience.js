f3d24674024c7b0ea8a50a03ce6ca938502a460d
SHAHARIYER AHAMMED <shahariyer.as@gmail.com>
2022-10-30 15:34:00 +0600

    some fixed

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,34 +1,38 @@
-import React,{useContext} from 'react';
-
+import React, { useContext } from 'react';
+import experience from '../../assets/lottie/development.json';
 import { ThemeContext } from '../../contexts/ThemeContext';
 
 import './Experience.css';
 
-import { experienceData } from '../../data/experienceData'
+import { Container } from '@material-ui/core';
+import { experienceData } from '../../data/experienceData';
+import AnimationLottie from '../Animation';
 import ExperienceCard from './ExperienceCard';
 
 function Experience() {
 
     const { theme } = useContext(ThemeContext);
     return (
-        <div className="experience" id="experience" style={{backgroundColor: theme.secondary}}> 
-             <div className="experience-body">
-                 <div className="experience-image">
-                     <img src={theme.expimg} alt="" />
-                 </div>
-                 <div className="experience-description">
-                    <h1 style={{color:theme.primary}}>Experience</h1>
-                    {experienceData.map(exp =>(
-                        <ExperienceCard 
-                            key={exp.id}
-                            id={exp.id}
-                            jobtitle={exp.jobtitle}
-                            company={exp.company}
-                            startYear={exp.startYear}
-                            endYear={exp.endYear}/>
-                    ))}
-                 </div>
-             </div>
+        <div style={{ backgroundColor: theme.secondary }}>
+            <Container className="experience" id="experience">
+                <div className="experience-body">
+                    <div className="experience-image">
+                        <AnimationLottie animationPath={experience} />
+                    </div>
+                    <div className="experience-description">
+                        <h1 style={{ color: theme.primary }}>Experience</h1>
+                        {experienceData.map(exp => (
+                            <ExperienceCard
+                                key={exp.id}
+                                id={exp.id}
+                                jobtitle={exp.jobtitle}
+                                company={exp.company}
+                                startYear={exp.startYear}
+                                endYear={exp.endYear} />
+                        ))}
+                    </div>
+                </div>
+            </Container>
         </div>
     )
 }
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,23 @@
 export const experienceData = [
-    {
-        id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
-    },
+    // {
+    //     id: 1,
+    //     company: '',
+    //     jobtitle: 'Software Engineer I',
+    //     startYear: 'Jan 2022',
+    //     endYear: 'Present'
+    // },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
+        company: 'Fiverr(freelance)',
+        jobtitle: 'Full-Stack Developer',
+        startYear: 'Jun 2021',
+        endYear: 'Jan 2022'
     },
     {
         id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
+        company: 'Learning New Technology For Everyday',
+        jobtitle: 'Self Learning',
+        startYear: '2018',
         endYear: 'Present'
     },
 ]
