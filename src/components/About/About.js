bd274915566131ffdd998a9a226d8d63e67dbfb0
SHAHARIYER AHAMMED <shahariyer.as@gmail.com>
2022-10-30 15:19:57 +0600

    about section done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,11 @@
+import { Container } from '@material-ui/core';
 import React, { useContext } from 'react';
+import codings from '../../assets/lottie/coding.json';
+import { ThemeContext } from '../../contexts/ThemeContext';
+import { aboutData } from '../../data/aboutData';
+import AnimationLottie from '../Skills/Animation';
 
 import './About.css';
-import { ThemeContext } from '../../contexts/ThemeContext';
-import { aboutData } from '../../data/aboutData'
 
 
 
@@ -10,26 +13,24 @@ function About() {
 
     const { theme } = useContext(ThemeContext);
     return (
-        <div className="about" id="about" style={{backgroundColor: theme.secondary}}>
-            <div className="line-styling">
-              <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
-              <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
-              <div className="style-line" style={{backgroundColor: theme.primary}}></div>
-            </div>
-            <div className="about-body">
-                <div className="about-description">
-                    <h2 style={{color: theme.primary}}>{aboutData.title}</h2>
-                    <p style={{color:theme.tertiary80}}>{aboutData.description1}<br/><br/>{aboutData.description2}</p>
+        <div style={{ backgroundColor: theme.secondary }}>
+            <Container className="about" id="about">
+                <div className="line-styling">
+                    <div className="style-circle" style={{ backgroundColor: theme.primary }}></div>
+                    <div className="style-circle" style={{ backgroundColor: theme.primary }}></div>
+                    <div className="style-line" style={{ backgroundColor: theme.primary }}></div>
                 </div>
-                <div className="about-img">
-                    <img 
-                        src={aboutData.image === 1 ? theme.aboutimg1 : theme.aboutimg2}  
-                        alt="" 
-                    />
+                <div className="about-body">
+                    <div className="about-description">
+                        <h2 style={{ color: theme.primary }}>{aboutData.title}</h2>
+                        <p style={{ color: theme.tertiary }}>{aboutData.description1}<br /><br />{aboutData.description2}</p>
+                    </div>
+                    <div className="about-animation">
+                        <AnimationLottie animationPath={codings} />
+                    </div>
                 </div>
-            </div>
+            </Container>
         </div>
-
     )
 }
 
