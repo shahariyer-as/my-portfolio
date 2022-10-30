d0aae4a821967c757d9384b15680bfbc355f29e6
SHAHARIYER AHAMMED <shahariyer.as@gmail.com>
2022-10-30 16:28:30 +0600

    themes add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
-import { ThemeContext } from './contexts/ThemeContext';
-import { Main, BlogPage, ProjectPage } from './pages'
-import { BackToTop } from './components'
-import ScrollToTop from './utils/ScrollToTop'
+import { BackToTop } from './components';
+import { BlogPage, Main, ProjectPage } from './pages';
+import ScrollToTop from './utils/ScrollToTop';
 
-import './App.css'
+import './App.css';
+import ChangeTheme from './components/ChangeTheme/ChangeTheme';
 
 function App() {
-
-  const { theme } = useContext(ThemeContext);
-
-  console.log("%cDEVELOPER PORTFOLIO", `color:${theme.primary}; font-size:50px`);
-  console.log("%chttps://github.com/hhhrrrttt222111/developer-portfolio", `color:${theme.tertiary}; font-size:20px`);
-  // console.log = console.warn = console.error = () => {};
 
   return (
     <div className="app">
       <Router>
-        <ScrollToTop/>
+        <ScrollToTop />
         <Switch>
           <Route path="/" exact component={Main} />
           <Route path="/blog" exact component={BlogPage} />
@@ -29,6 +23,7 @@ function App() {
         </Switch>
       </Router>
       <BackToTop />
+      <ChangeTheme />
     </div>
   );
 }
